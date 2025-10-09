dfeb09f6877ac0edb28347da210c7577af5c42b0
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-10-08 20:43:51 -0500

    Create FuncionesAuxiliares.h

---
+++ PracticarLabs/LAB05/LAB05_25-2_Virtual/Bibliotecas/FuncionesAuxiliares.h
@@ -0,0 +1,64 @@
+//
+// Created by DIEGO on 10/6/2025.
+//
+
+#ifndef INC_2025_2_LAB05_ARREGLOS_ORDENACION_FUNCIONESAUXILIARES_H
+#define INC_2025_2_LAB05_ARREGLOS_ORDENACION_FUNCIONESAUXILIARES_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+#define MAX_CANALES 120
+#define MAX_ETIQUETAS 120
+#define MAX_ETIQUETA_CANAL 600
+
+//Modulo apertura de archivos y preguntarTarifa
+void aperturaArchivoLectura(const char* nombArch,ifstream& arch);
+void aperturaArchivoEscritura(const char* nombArch,ofstream& arch);
+void preguntarTarifa(double &tarifaDur);
+
+//Modulos principales
+void cargarCanales(const char* nombArchCanales,int *arrFechaCanal,
+                    char *arrCodeCharCanal,int *arrCodeIntCanal,
+                    double *arrRatingCanal,int &nroCanales);
+void pruebaCanales(const char* nombArchRepPruebaCanales,int *arrFechaCanal,
+                    char *arrCodeCharCanal,int *arrCodeIntCanal,
+                    double *arrRatingCanal,int nroCanales);
+
+
+void cargarEtiquetas(const char* nombArchEtiquetas,int* arrCodeEtiqueta,
+                    int* arrTiempoDurEtiqueta,int& nroEtiquetas);
+void pruebaEtiquetas(const char* nombArchRepPruebaEtiquetas,int* arrCodeEtiqueta,
+                    int* arrTiempoDurEtiqueta,int nroEtiquetas);
+
+void cargarEtiquetaCanal(const char* nombArchEtiquetaCanales,
+                        char *arrCodeCharEtiquetaCanal,int *arrCodeIntEtiquetaCanal,
+                        int *arrCodeEtiquetaCanal,int &nroEtiquetaCanal);
+void pruebaEtiquetaCanal(const char* nombArchRepPruebaEtiCanal,
+                        char *arrCodeCharEtiquetaCanal,int *arrCodeIntEtiquetaCanal,
+                        int *arrCodeEtiquetaCanal,int nroEtiquetaCanal);
+
+void procesarReproEtiquetas(const char* nombArchReproEtiquetas,
+                            int *arrFechaCanal,char *arrCodeCharCanal,int *arrCodeIntCanal,
+                            double *arrRatingCanal,int nroCanales,
+                            int *arrCodeEtiqueta,int *arrTiempoDurEtiqueta,int nroEtiquetas,
+                            char *arrCodeCharEtiquetaCanal,int *arrCodeIntEtiquetaCanal,
+                            int *arrCodeEtiquetaCanal,int nroEtiquetaCanal,
+                            int *arrNroTotalReproCanal,int *arrTiempoTotalReproCanal,
+                            int *arrNroTotalReproCanalGlobal,int *arrTiempoTotalReproCanalGlobal);
+
+int buscarCanal(char *arrCodeCharCanal,int *arrCodeIntCanal,
+                char codeCharCanalArch,int codeIntCanalArch,int nroEtiquetaCanal);
+
+
+//Modulos auxiliares
+int leerTiempo(ifstream& input);
+void imprimirTiempo(ofstream& output,int tiempo,bool esMinuto);
+void imprimirNroOrden(ofstream& output,int nroOrden) ;
+void imprimirLineas(char c,int cant,ofstream& output);
+void ignorar(ifstream& input,char delimitador);
+int leerFecha(ifstream& input);
+void imprimirFecha(ofstream& output,int fecha);
+
+#endif //INC_2025_2_LAB05_ARREGLOS_ORDENACION_FUNCIONESAUXILIARES_H
