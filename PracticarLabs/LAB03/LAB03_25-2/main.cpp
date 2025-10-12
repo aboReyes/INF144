fde757d4834e14ac0da1ea8c01fa684a8438d5d0
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-10-12 02:07:38 -0500

    Create main.cpp

---
+++ PracticarLabs/LAB03/LAB03_25-2/main.cpp
@@ -0,0 +1,17 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo:
+*Fecha y Hora: 2 horas bien hechas, podria haber si 1:30 :u
+*/
+
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+
+
+    generarReporte("ArchivoDeDatos/Canales (4).txt",
+                    "ArchivoDeDatos/Etiquetas (4).txt",
+                    "ArchivoDeDatos/ReproduccionesDeEtiquetas (4).txt",
+                    "ArchivoDeReporte/ReporteTwitch.txt");
+    return 0;
+}
