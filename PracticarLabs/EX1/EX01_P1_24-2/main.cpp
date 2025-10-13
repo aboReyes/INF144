a0ad8277f834a7845d89fd46e6f02c088718d327
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-10-12 19:29:21 -0500

    Create main.cpp

---
+++ PracticarLabs/EX1/EX01_P1_24-2/main.cpp
@@ -0,0 +1,18 @@
+/*
+*Autor: Diego Axel Lopez Reyes
+*Titulo: .....
+*Descripcion:
+*Fecha y Hora:
+*/
+
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main(int argc, char **argv) {
+
+    generarReporte("ArchivoDeDatos/Clientes (3).txt",
+                    "ArchivoDeDatos/PlatosOfrecidos (3).txt",
+                    "ArchivoDeDatos/Repartidores (3).txt",
+                    "ArchivoDeDatos/RepartosARealizar (3).txt",
+                    "ArchivoDeReporte/ReporteEmpresaRepartos.txt");
+    return 0;
+}
