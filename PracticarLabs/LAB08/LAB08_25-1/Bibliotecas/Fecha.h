7b73b25b8569a09b791688591e9212f4ebbfd613
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-09 21:40:40 -0500

    Add Fecha.h header file for date structure

---
+++ PracticarLabs/LAB08/LAB08_25-1/Bibliotecas/Fecha.h
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 9/11/2025.
+//
+
+#ifndef LAB08_25_1_FECHA_H
+#define LAB08_25_1_FECHA_H
+
+struct Fecha {
+    int dd;
+    int mm;
+    int aa;
+    int fecha;
+};
+
+#endif //LAB08_25_1_FECHA_H
