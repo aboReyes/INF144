f16ae6f816cfbc71b91bf281620c33054122b872
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-10 12:42:06 -0500

    Create Plato.h

---
+++ PracticarLabs/LAB08/LAB08_24-2/Bibliotecas/Plato.h
@@ -0,0 +1,16 @@
+//
+// Created by ASUS on 7/11/2025.
+//
+
+#ifndef LAB08_24_2_PLATO_H
+#define LAB08_24_2_PLATO_H
+
+struct Plato {
+  char codigo[20];
+  char descripcion[100];
+  double precio;
+  char categoria[50];
+  int cantidad;
+};
+
+#endif //LAB08_24_2_PLATO_H
