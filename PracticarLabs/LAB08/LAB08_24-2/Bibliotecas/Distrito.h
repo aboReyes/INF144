59f5dcd1f02c9067272135fc807c02e87769b19c
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-10 12:42:44 -0500

    Create Distrito.h

---
+++ PracticarLabs/LAB08/LAB08_24-2/Bibliotecas/Distrito.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 7/11/2025.
+//
+
+#ifndef LAB08_24_2_DISTRITO_H
+#define LAB08_24_2_DISTRITO_H
+
+struct Distrito {
+    char codigo[20];
+    char nombre[50];
+    char ubicacion[20];
+};
+
+#endif //LAB08_24_2_DISTRITO_H
