7d38259492319a7a3c86fe4ae3ea42519fdb4822
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-16 18:22:44 -0500

    Create TablaDeInfracciones.h

---
+++ PracticarLabs/LAB09/LAB09_25-1/Bibliotecas/TablaDeInfracciones.h
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 16/11/2025.
+//
+
+#ifndef LAB09_25_1_TABLADEINFRACCIONES_H
+#define LAB09_25_1_TABLADEINFRACCIONES_H
+
+//INFORMACION DE UNA INFRACCION (SU CODIGO, PRECIO,ETC)
+struct TablaDeInfracciones {
+    char *codigo;
+    double multa;
+    char* descripcion;
+};
+
+#endif //LAB09_25_1_TABLADEINFRACCIONES_H
