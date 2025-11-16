181f6aed1158120613ff2e2d677f562d006eea34
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-16 18:22:18 -0500

    Create Fecha.h

---
+++ PracticarLabs/LAB09/LAB09_25-1/Bibliotecas/Fecha.h
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 16/11/2025.
+//
+
+#ifndef LAB09_25_1_FECHA_H
+#define LAB09_25_1_FECHA_H
+
+struct Fecha {
+    int dd;
+    int mm;
+    int aa;
+    int fecha;
+};
+
+#endif //LAB09_25_1_FECHA_H
