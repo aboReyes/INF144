3fe29b950677b107569630790a9438c23285dd5d
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-21 12:49:25 -0500

    Create LibrosEnSuPoder.h

---
+++ PracticarLabs/EX2/Bibliotecas/LibrosEnSuPoder.h
@@ -0,0 +1,13 @@
+//
+// Created by aml on 21/11/2025.
+//
+
+#ifndef LIBROENSUPODER_H
+#define LIBROENSUPODER_H
+
+struct LibroEnSuPoder {
+    char* codLibro;
+    int fechaDevolucion;
+};
+
+#endif //LIBROENSUPODER_H
