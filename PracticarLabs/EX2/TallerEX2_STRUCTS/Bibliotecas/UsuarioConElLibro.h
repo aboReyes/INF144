f9877a08173fd8ece8a58e49bfed229474790e6b
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-21 12:52:37 -0500

    Create UsuarioConElLibro.h

---
+++ PracticarLabs/EX2/TallerEX2_STRUCTS/Bibliotecas/UsuarioConElLibro.h
@@ -0,0 +1,13 @@
+//
+// Created by aml on 21/11/2025.
+//
+
+#ifndef USUARIOCONELLIBRO_H
+#define USUARIOCONELLIBRO_H
+
+struct UsuarioConElLibro {
+    int dni;
+    int fechaDeDevolucion;
+};
+
+#endif //USUARIOCONELLIBRO_H
