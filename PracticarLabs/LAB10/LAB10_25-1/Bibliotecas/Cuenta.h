0065d7d93dd1cbbf9a95d0a28d0eec672d2b32f1
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-24 14:13:56 -0500

    Create Cuenta.h

---
+++ PracticarLabs/LAB10/LAB10_25-1/Bibliotecas/Cuenta.h
@@ -0,0 +1,17 @@
+//
+// Created by ASUS on 24/11/2025.
+//
+
+#ifndef LAB10_25_1_CUENTA_H
+#define LAB10_25_1_CUENTA_H
+
+struct Cuenta {
+    int codigo;
+    char* nombre;
+    double saldo;
+    int fechaSobregiro;
+    bool sobregiro;
+
+};
+
+#endif //LAB10_25_1_CUENTA_H
