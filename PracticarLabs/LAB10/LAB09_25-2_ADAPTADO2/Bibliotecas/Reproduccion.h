8f954dda9df05a650a1060267c925229ff3cb59e
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-23 22:08:37 -0500

    Create Reproduccion.h

---
+++ PracticarLabs/LAB10/LAB09_25-2_ADAPTADO2/Bibliotecas/Reproduccion.h
@@ -0,0 +1,16 @@
+//
+// Created by ASUS on 23/11/2025.
+//
+
+#ifndef REPASOLAB09_25_2_REPRODUCCION_H
+#define REPASOLAB09_25_2_REPRODUCCION_H
+
+struct Reproduccion {
+    char* canal;
+    double rating;
+    int duracion;
+    char* comentarios[20];
+    int numComentarios;
+};
+
+#endif //REPASOLAB09_25_2_REPRODUCCION_H
