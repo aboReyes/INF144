4e07a071a8d7465228c9d668837c832a1fc3ce79
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-28 12:42:32 -0500

    Create NodoUsuarioAmoroso.h

---
+++ PracticarLabs/EX2/TALLER_LISTAS/Bibliotecas/NodoUsuarioAmoroso.h
@@ -0,0 +1,13 @@
+//
+// Created by aml on 28/11/2025.
+//
+
+#ifndef NODOUSUARIOMOROSO_H
+#define NODOUSUARIOMOROSO_H
+
+struct NodoUsuarioMoroso {
+    int dni;
+    NodoUsuarioMoroso* siguiente;
+};
+
+#endif //NODOUSUARIOMOROSO_H
