2f23820a3733494baeba72dd3b7c62b14ceee9cd
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-27 23:41:57 -0500

    Update main.cpp

--- PracticarLabs/EX2/TALLER_STRUCTS/main.cpp
+++ PracticarLabs/EX2/TALLER_STRUCTS/main.cpp
@@ -14,11 +14,28 @@ int main() {
     leerLibros("ArchivoDeDatos/Libros.csv",
                 arrLibros,cantidadLibros);
 
-    pruebaLibros("ArchivoDeReportes/Inicial.libros.txt",
-                arrLibros,cantidadLibros);
+    pruebaLibros("ArchivoDeReportes/InicialLibros.txt",
+                arrLibros,cantidadLibros,false);
 
     leerUsuarios("ArchivoDeDatos/Usuarios.csv",
                 arrUsuarios,cantidadUsuarios);
+
+    pruebaUsuarios("ArchivoDeReportes/InicialUsuarios.txt",
+                arrUsuarios,cantidadUsuarios,false);
+
+    actualizarPrestamos("ArchivoDeDatos/PrestamoDeLibros.txt",
+                arrLibros,cantidadLibros,arrUsuarios,cantidadUsuarios);
+
+    pruebaLibros("ArchivoDeReportes/LibrosConPrestamo.txt",
+                arrLibros,cantidadLibros,true);
+
+    pruebaUsuarios("ArchivoDeReportes/UsuariosConPrestamos.txt",
+                arrUsuarios,cantidadUsuarios,true);
+
+    ordenarReporte(arrLibros,cantidadLibros);
+
+    pruebaLibros("ArchivoDeReportes/LibrosOrdenado.txt",
+                arrLibros,cantidadLibros,true);
 
     return 0;
 }
