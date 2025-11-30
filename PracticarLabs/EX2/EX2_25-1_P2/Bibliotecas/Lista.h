e8c2a404d7dd84c736affde3142c1c8cc2fde98f
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-30 15:15:14 -0500

    Create Lista.h

---
+++ PracticarLabs/EX2/EX2_25-1_P2/Bibliotecas/Lista.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_25_1_P2_LISTA_H
+#define EX2_25_1_P2_LISTA_H
+#include "Nodo.h"
+
+struct Lista {
+    Nodo* inicio;
+    Nodo* fin;
+};
+
+#endif //EX2_25_1_P2_LISTA_H
