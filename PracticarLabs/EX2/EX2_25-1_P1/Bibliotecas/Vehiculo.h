eee49f46e4c39b66b7de40d4c640f0d9267aa759
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-30 11:41:37 -0500

    Create Vehiculo.h

---
+++ PracticarLabs/EX2/EX2_25-1_P1/Bibliotecas/Vehiculo.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_25_1_P1_VEHICULO_H
+#define EX2_25_1_P1_VEHICULO_H
+
+struct Vehiculo {
+    int dniPropietario;
+    char *placa;
+    char *marcaModelo;
+};
+
+#endif //EX2_25_1_P1_VEHICULO_H
