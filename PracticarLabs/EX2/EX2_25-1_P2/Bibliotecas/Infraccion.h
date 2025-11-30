f7b8ac40bbc4128a84b930459b0dd7896654f35a
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-30 15:14:52 -0500

    Create Infraccion.h

---
+++ PracticarLabs/EX2/EX2_25-1_P2/Bibliotecas/Infraccion.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_25_1_P2_INFRACCION_H
+#define EX2_25_1_P2_INFRACCION_H
+
+struct Infraccion {
+    int codigo;
+    char* descripcion;
+    char* gravedad;
+};
+
+#endif //EX2_25_1_P2_INFRACCION_H
