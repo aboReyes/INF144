73899e86ef96d98872359f713cca9f1008552664
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-30 15:15:03 -0500

    Create Nodo.h

---
+++ PracticarLabs/EX2/EX2_25-1_P2/Bibliotecas/Nodo.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_25_1_P2_NODO_H
+#define EX2_25_1_P2_NODO_H
+#include "Infraccion.h"
+
+struct Nodo {
+    Infraccion infraccion;
+    Nodo* siguiente;
+};
+
+#endif //EX2_25_1_P2_NODO_H
