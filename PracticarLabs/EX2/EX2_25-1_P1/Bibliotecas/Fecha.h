2d22893cdade66091d2fc21b18f320d47a477945
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-11-30 11:41:23 -0500

    Create Fecha.h

---
+++ PracticarLabs/EX2/EX2_25-1_P1/Bibliotecas/Fecha.h
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_25_1_P1_FECHA_H
+#define EX2_25_1_P1_FECHA_H
+
+struct Fecha {
+    int aa;
+    int mm;
+    int dd;
+    int aammdd;
+};
+
+#endif //EX2_25_1_P1_FECHA_H
