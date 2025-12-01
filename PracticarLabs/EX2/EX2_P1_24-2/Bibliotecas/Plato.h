98a250c1cbfac7659ce77c178d5deef1d65e7697
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-12-01 09:30:24 -0500

    Create Plato.h

---
+++ PracticarLabs/EX2/EX2_P1_24-2/Bibliotecas/Plato.h
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_24_2_P1_PLATO_H
+#define EX2_24_2_P1_PLATO_H
+
+struct Plato {
+    char* codigo;
+    char* nombre;
+    double precio;
+    int cantidad;
+};
+
+#endif //EX2_24_2_P1_PLATO_H
