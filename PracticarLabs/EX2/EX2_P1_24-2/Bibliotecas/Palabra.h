225fe01a5f556e3b31ed92ff4091120bbb18ab79
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-12-01 09:30:00 -0500

    Create Palabra.h

---
+++ PracticarLabs/EX2/EX2_P1_24-2/Bibliotecas/Palabra.h
@@ -0,0 +1,13 @@
+//
+// Created by ASUS on 30/11/2025.
+//
+
+#ifndef EX2_24_2_P1_PALABRA_H
+#define EX2_24_2_P1_PALABRA_H
+
+struct Palabra {
+    char* texto;
+    int polaridad;
+};
+
+#endif //EX2_24_2_P1_PALABRA_H
