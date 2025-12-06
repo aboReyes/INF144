2797091651407082abcd72ac88a124661efa2644
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-12-05 19:17:17 -0500

    Create Comentario.h

---
+++ PracticarLabs/EX2/EX2_25-2/Bibliotecas/Comentario.h
@@ -0,0 +1,15 @@
+//
+// Created by DIEGO on 05/12/2025.
+//
+
+#ifndef P1_COMENTARIO_H
+#define P1_COMENTARIO_H
+
+struct Comentario {
+    int id_transmision;
+    char* comentador;
+    char* comentario_original;
+    char* comentario_pre_procesado;
+};
+
+#endif //P1_COMENTARIO_H
