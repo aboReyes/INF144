460497d3dd5e75b1575f2f6742c9c052d90d4ba6
Diego Reyes <128253333+aboReyes@users.noreply.github.com>
2025-12-05 19:17:42 -0500

    Add Streamer struct definition in Streamer.h

---
+++ PracticarLabs/EX2/EX2_25-2/Bibliotecas/Streamer.h
@@ -0,0 +1,15 @@
+//
+// Created by DIEGO on 05/12/2025.
+//
+
+#ifndef P1_STREAMER_H
+#define P1_STREAMER_H
+
+struct Streamer {
+    int id_streamer;
+    char* nombre_canal;
+    char* categoria;
+    int cantidad_seguidores;
+};
+
+#endif //P1_STREAMER_H
